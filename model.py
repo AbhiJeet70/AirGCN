7bc1a3abb468ab4f8e282400bdb79b61840315df
Abhijeet Dhali <72669714+AbhiJeet70@users.noreply.github.com>
2024-07-02 14:57:37 -0400

    Create model.py

---
+++ model.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
+
+# Define the GCN model with increased complexity and batch normalization
+class GCNNet(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels):
+        super(GCNNet, self).__init__()
+        self.conv1 = GCNConv(in_channels, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, hidden_channels)
+        self.conv3 = GCNConv(hidden_channels, hidden_channels)
+        self.conv4 = GCNConv(hidden_channels, hidden_channels)
+        self.conv5 = GCNConv(hidden_channels, out_channels)
+        self.bn1 = torch.nn.BatchNorm1d(hidden_channels)
+        self.bn2 = torch.nn.BatchNorm1d(hidden_channels)
+        self.bn3 = torch.nn.BatchNorm1d(hidden_channels)
+        self.bn4 = torch.nn.BatchNorm1d(hidden_channels)
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index)
+        x = self.bn1(x)
+        x = F.relu(x)
+        x = F.dropout(x, p=0.5, training=self.training)
+        
+        x = self.conv2(x, edge_index)
+        x = self.bn2(x)
+        x = F.relu(x)
+        x = F.dropout(x, p=0.5, training=self.training)
+        
+        x = self.conv3(x, edge_index)
+        x = self.bn3(x)
+        x = F.relu(x)
+        x = F.dropout(x, p=0.5, training=self.training)
+        
+        x = self.conv4(x, edge_index)
+        x = self.bn4(x)
+        x = F.relu(x)
+        x = F.dropout(x, p=0.5, training=self.training)
+        
+        x = self.conv5(x, edge_index)
+        return F.log_softmax(x, dim=1)
